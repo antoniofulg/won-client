22c9aab9be49ca4d45e3a9edee8c9bf25b26f423
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-11-19 01:02:36 -0300

    added banner and ribbon integration

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -11,6 +11,25 @@ export default {
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death',
   },
+  parameters: {
+    layout: 'fullscreen',
+  },
 } as Meta
 
-export const Basic: Story<BannerProps> = (args) => <Banner {...args} />
+export const Basic: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary',
+}
