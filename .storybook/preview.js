e70caa70d9c3336906e0c0fa6496b408fb18b845
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-11-23 22:11:46 -0300

    added background rules for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,26 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        values: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        values: theme.colors.mainBg
+      },
+    ],
+  },
+};
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg/>
       <Story />
     </ThemeProvider>
   ),
