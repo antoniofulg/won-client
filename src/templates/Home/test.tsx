6ceb2e4f48e6e446c4e04abed1c111fdfc1038cd
Antonio <antoniofulg@gmail.com>
2021-12-10 01:37:18 -0300

    optimization of test

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,13 +10,13 @@ import Home from '.'
 
 const props = {
   banners: bannerMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcomingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
   upcomingHighlight: highlightMock,
-  upcomingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightMock,
 }
 
@@ -25,33 +25,31 @@ describe('<Home />', () => {
     renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /contact/i })
     ).toBeInTheDocument()
-    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-  })
 
-  it('should render sections', () => {
-    renderWithTheme(<Home {...props} />)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /most popular/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /upcoming/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />)
-    // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
-    // highlight
+
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+
     expect(screen.getAllByText(/red dead it's back/i)).toHaveLength(3)
   })
 })
