16a3f0ea73f280d1215c64fada943eb1f50f2761
Antonio <antoniofulg@gmail.com>
2021-12-10 17:21:33 -0300

    added accessibility test

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -4,13 +4,43 @@ import Checkbox, { CheckboxProps } from '.'
 export default {
   title: 'Checkbox',
   component: Checkbox,
-  argTypes: {
-    onCheck: {
-      action: 'checked',
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark',
     },
+  },
+  argTypes: {
+    onCheck: { action: 'checked' },
   },
 } as Meta
 
 export const Basic: Story<CheckboxProps> = (args) => (
-  <Checkbox isChecked {...args} />
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+        {...args}
+      />
+    </div>
+  </>
 )
