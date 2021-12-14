8a497c5a4b4554940ced7aa4898070f280a2e5f4
Antonio <antoniofulg@gmail.com>
2021-12-13 22:25:20 -0300

    created auth structure

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,11 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /all your favorite games in on place/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform./i,
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /auth title/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
