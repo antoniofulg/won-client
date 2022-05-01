6c48bddd86e176b307de9cab312459f3658a63b9
Antonio <antoniofulg@gmail.com>
2022-05-01 16:39:18 -0300

    created grid component

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,18 +1,42 @@
-import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
+import Base from 'templates/Base'
 
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { Container } from 'components/Container'
+import { Grid } from 'components/Grid'
+import { HighlightProps } from 'components/Highlight'
 
-export default function WishlistPage(props: WishlistTemplateProps) {
-  return <Wishlist {...props} />
+export type WishlistTemplateProps = {
+  games?: GameCardProps[]
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
 }
 
-export async function getStaticProps() {
-  return {
-    props: {
-      games: gamesMock,
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: highlightMock,
-    },
-  }
-}
+const Wishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight,
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
+export default Wishlist
