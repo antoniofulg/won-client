b54b4977707fa3b732fcdc23bc51e07882e16af1
Antonio <antoniofulg@gmail.com>
2022-05-09 00:44:49 -0300

    introduced graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,41 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `,
+  })
+
   return <Home {...props} />
 }
 
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
 export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
   return {
     props: {
       banners: bannersMock,
