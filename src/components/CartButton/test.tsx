ead57a5f385c78c5ff7294a97e46eb44478f68ee
Antonio <antoniofulg@gmail.com>
2022-07-17 04:37:07 -0300

    add waitfor on test

--- src/components/CartButton/test.tsx
+++ src/components/CartButton/test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from 'utils/test-utils'
+import { render, screen, waitFor } from 'utils/test-utils'
 import { CartContextDefaultValues } from 'hooks/use-cart'
 
 import CartButton from '.'
 import userEvent from '@testing-library/user-event'
 
 describe('<CartButton />', () => {
-  it('should render button to add and call the method if clicked', () => {
+  it('should render button to add and call the method if clicked', async () => {
     const cartProviderProps = {
       ...CartContextDefaultValues,
       isInCart: () => false,
@@ -18,10 +18,13 @@ describe('<CartButton />', () => {
     expect(button).toBeInTheDocument()
 
     userEvent.click(button)
-    expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
+
+    await waitFor(() => {
+      expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
+    })
   })
 
-  it('should render button to remove and call the method if clicked', () => {
+  it('should render button to remove and call the method if clicked', async () => {
     const cartProviderProps = {
       ...CartContextDefaultValues,
       isInCart: () => true,
@@ -34,6 +37,9 @@ describe('<CartButton />', () => {
     expect(button).toBeInTheDocument()
 
     userEvent.click(button)
-    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
+
+    await waitFor(() => {
+      expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
+    })
   })
 })
